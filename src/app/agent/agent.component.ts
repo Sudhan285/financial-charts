62dfb62fb9f6a492012b0a32719be667d58cc5e0
Sam Chapman <schap843@gmail.com>
2016-12-07 16:37:08 +0900

    initial currency commit

--- src/app/agent/agent.component.ts
+++ src/app/agent/agent.component.ts
@@ -1,70 +1,71 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ChatService }       from '../services/chat.service';
+import { Component, OnInit } from '@angular/core';
 import { AlertComponent } from '../shared/alert/alert.component';
-import {AuthService} from '../services/auth.service';
+import { DataService } from '../services/data.service';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 @Component({
   selector: 'app-agent',
   templateUrl: './agent.component.html',
   styleUrls: ['./agent.component.css'],
-  providers: [ChatService, AuthService]
+  providers: [DataService]
 })
 export class AgentComponent implements OnInit {
 
-  public stockCode:string;
-  public stocks:Array<any> = [{code:'IBM',name:'International Business Machines'}];
+  public currencies:any = [{code:'AUD',name:'Australian Dollar'},{code:'BGN',name:'Bulgarian Lev'},{code:'BRL',name:'Brazilian Real'},
+                          {code:'CAD',name:'Canadian Dollar'},{code:'CHF',name:'Swiss Franc'},{code:'CNY',name:'Chinese Yuan Renminbi'},{code:'CZK',name:'Czech Koruna'},{code:'DKK',name:'Danish Krone'},{code:'EUR',name:'Euro'},{code:'GBP',name:'Great Britain Pound'},{code:'HKD',name:'Hong Kong Dollar'},
+                          {code:'HRK',name:'Croatian Kuna'},{code:'HUF',name:'Hungarian Forint'},{code:'IDR',name:'Indonesian Rupiah'},{code:'ILS',name:'Israeli Shekel'},{code:'INR',name:'Indian Rupee'},{code:'JPY',name:'Japanese Yen'},{code:'KRW',name:'South Korean Won'},{code:'MXN',name:'Mexican Peso'},
+                          {code:'MYR',name:'Malaysian Ringgit'},{code:'NOK',name:'Norwegian Krone'},{code:'NZD',name:'New Zealand Dollor'},{code:'PHP',name:'Philippine Peso'},{code:'PLN',name:'Polish Zloty'},{code:'RON',name:'Romanian New Leu'},{code:'RUB',name:'Russian Ruble'},{code:'SEK',name:'Swedish Krona'},
+                          {code:'SGD',name:'Singapore Dollar'},{code:'THB',name:'Thai Baht'},{code:'TRY',name:'Turkish Lira'},{code:'USD',name:'United States Dollar'},{code:'ZAR',name:'South African Rand'}]
 
-  constructor() { }
+  public base:string = 'USD';
+  public curr:string = 'EUR';
+  public multiplier:number = 1;
+  public currHistory:Array<any>;
+  public rate:number;
+
+  constructor(private data:DataService) { }
 
   ngOnInit() {
+    this.dates();
   }
 
-  public addStock(){
-    let obj = {code:'IBM',name:'Int Biz Mach'};
-    console.log(this.stockCode);
-    this.stockCode = '';
-    this.stocks.push(obj);
-  };
-  public deleteStock(stock){
-    let x = this.stocks.indexOf(stock);
-    this.stocks.splice(x,1);
-  };
-  // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', fill:false},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', fill:false},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C', fill:false},
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series D', fill:false},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series E', fill:false},
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series F', fill:false},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series G', fill:false}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    animation: false,
-    responsive: true
-  };
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
-
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label, fill:false};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
+  public dates(){
+    // 1m, 3m, 6m, 9m, 1y
+    let dateArr = ['latest'];
+    function addMonths(date, months) {
+      date.setMonth(date.getMonth() + months);
+      return date;
     }
-    this.lineChartData = _lineChartData;
+    function formatDate(date){
+      let year = date.getFullYear();
+      let month = (date.getMonth() + 1)<10 ? '0'+(date.getMonth() + 1) : date.getMonth() + 1;
+      let day = date.getDate()<10 ? '0'+date.getDate() : date.getDate();
+      let x = `${year}-${month}-${day}`;
+      dateArr.push(x);
+    }
+    this.currHistory = [];
+    formatDate(addMonths(new Date(), -1));
+    formatDate(addMonths(new Date(), -3));
+    formatDate(addMonths(new Date(), -6));
+    formatDate(addMonths(new Date(), -9));
+    formatDate(addMonths(new Date(), -12));
+    for (let date of dateArr){
+      this.findRates(date);
+    }
   }
 
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
+  public findRates(date){
+    let tempArr=[];
+    this.data.getCurrency(this.base,date)
+    .subscribe(
+      stock => {
+        let obj={date:stock.date,base:stock.base,curr:stock.rates[this.curr]};
+        tempArr.push(obj);
+    },
+      error => console.log(error),
+    );
+    console.log(tempArr[0]);
+  };
 
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
+
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,12 +8,18 @@ import 'rxjs/add/operator/map';
 export class DataService {
   private apiKey = "WTuGQxLsvq-PutMxZAPz";
   constructor(private http: Http) { }
+
   getStocks(name) {
     let now = new Date();
     let year = now.getFullYear();
     let month = now.getMonth() + 1;
     let date = now.getDate();
     let url = `https://www.quandl.com/api/v3/datasets/WIKI/${name}.json?api_key=${this.apiKey}&start_date=${year - 1}-${month}-${date}&end_date=${year}-${month}-${date}`
+    return this.http.get(url).map(res => res.json());
+  }
+
+  getCurrency(base, date) {
+    let url = `http://api.fixer.io/${date}?base=${base}`;
     return this.http.get(url).map(res => res.json());
   }
 }
